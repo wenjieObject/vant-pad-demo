574fd588d1c8fa07aa95573534d3b8b7479bbea4
unknown <jiangwj@yunsheng.local>
2021-05-25 10:04:34 +0800

    添加更新

--- src/views/user/Login.vue
+++ src/views/user/Login.vue
@@ -28,7 +28,7 @@
 <script>
 import { apiLogin, apiVersion } from "../../request/api"; // 导入我们的api接口
 import md5 from "js-md5";
-import { Dialog,Toast } from "vant";
+import { Dialog, Toast } from "vant";
 export default {
   data() {
     return {
@@ -42,7 +42,6 @@ export default {
   },
 
   methods: {
-    
     onSubmit(values) {
       //this.$router.push({ path: "/home" });
       //返回用户名存入
@@ -52,7 +51,7 @@ export default {
       }).then((res) => {
         // 获取数据成功后的其他操作
         if (res.StatusCode === 0) {
-          Toast.success('登录成功')
+          Toast.success("登录成功");
           sessionStorage.setItem("token", res.Data.F_UserId);
           sessionStorage.setItem("usename", res.Data.F_RealName);
           this.$router.push({ path: "/home" });
@@ -69,17 +68,19 @@ export default {
 
     getVersion() {
       apiVersion().then((res) => {
+        debugger;
         // 获取数据成功后的其他操作
         if (res.StatusCode === 0) {
-          this.versionFlag = true;
+          var currentVersion = window.plus.runtime.version;
+          console.log("当前版本" + currentVersion);
+          console.log("返回值" + res);
+
           Dialog.confirm({
             title: "系统更新",
             message: "更新内容为",
           }).then(() => {
- 
-            this.currentVersion = window.plus.runtime.version;
-            this.Website='http://10.12.100.208:9090/app/android_debug.apk';
-            window.plus.runtime.openURL(this.Website);
+            this.Website = "http://10.12.100.208:9090/app/android_debug.apk";
+            //window.plus.runtime.openURL(this.Website);
           });
         } else {
           Dialog.alert({
@@ -90,24 +91,68 @@ export default {
       });
     },
 
-    plusReady() {
-      var ws = window.plus.webview.currentWebview(); //pw回车可输出plus.webview
-      console.log(ws);
+    downWgt() {
+      let that = this;
+
+      const wgtUrl = "http://10.12.100.208:9090/app/android_debug.apk";
+      //
+      document.addEventListener("plusready", function () {
+        window.plus.nativeUI.showWaiting("下载更新");
+        var currentVersion = window.plus.runtime.version;
+        console.log("当前版本" + currentVersion);
+        window.plus.downloader
+          .createDownload(
+            wgtUrl,
+            { filename: "_doc/update/" },
+            function (d, status) {
+              if (status == 200) {
+                console.log("下载更新成功：" + d.filename);
+                that.installWgt(d.filename); // 安装wgt资源包
+              } else {
+                console.log("下载更新失败！");
+                window.plus.nativeUI.toast("下载更新失败！");
+              }
+              window.plus.nativeUI.closeWaiting();
+            }
+          )
+          .start();
+      });
+    },
+
+    installWgt(path) {
+      window.plus.nativeUI.showWaiting("安装更新");
+      window.plus.runtime.install(
+        path,
+        {},
+        function () {
+          window.plus.nativeUI.closeWaiting();
+          console.log("安装更新成功！");
+          window.plus.nativeUI.alert("更新完成！", function () {
+            //  更新完成后重启应用
+            window.plus.runtime.restart();
+          });
+        },
+        function (e) {
+          window.plus.nativeUI.closeWaiting();
+          console.log("安装更新失败！[" + e.code + "]：" + e.message);
+          window.plus.nativeUI.toast("安装更新失败！");
+        }
+      );
     },
   },
   mounted() {
     this.$store.commit("setBartitle", "登录");
 
     //自动更新apk
-    //this.getVersion();
+    this.downWgt();
   },
-  created() {
-    if (window.plus) {
-      this.plusReady();
-    } else {
-      document.addEventListener("plusready", this.plusReady, false);
-    }
-  },
+  // created() {
+  //   if (window.plus) {
+  //     this.plusReady();
+  //   } else {
+  //     document.addEventListener("plusready", this.plusReady, false);
+  //   }
+  // },
 };
 </script>
 
